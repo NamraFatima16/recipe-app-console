47418e9fc7b0f11df34dc3bb45a337c2e5fd7918
Namra <20094326@mail.wit.ie>
2022-05-01 14:56:32 +0100

    Updated .gitignore again and added dependencies and plugins to gradle build for  - a logger  - serializing data to xml and json  - linting using ktlint  - document generation using dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.0"
     application
+    id("org.jetbrains.dokka") version "1.6.10"           // Plugin for Dokka
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1" // Plugin for Ktlint
 }
 
 group = "me.namra"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -14,6 +16,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.thoughtworks.xstream:xstream:1.4.19")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.20")
+    // Dependencies for logging
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
 }
 
 tasks.test {
