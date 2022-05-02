67fb669c379621c39fc8cbc6e698d64640bc14ed
Namra <20094326@mail.wit.ie>
2022-05-02 15:43:44 +0100

    Added all of the CRUD methods for recipes in the recipeApi controller

--- src/main/kotlin/controllers/RecipeApi.kt
+++ src/main/kotlin/controllers/RecipeApi.kt
@@ -21,9 +21,86 @@ class RecipeApi {
         }
     }
 
+    fun listRecipesByMealType(mealType: String): String {
+        val count = recipes.count { recipe: Recipe -> recipe.mealType == mealType }
+        return if (count == 0) {
+            "No recipes for meal type '$mealType'"
+        } else {
+            val matches = recipes.filter { recipe: Recipe -> recipe.mealType == mealType }
+            matches.joinToString(separator = "\n") { recipe: Recipe ->
+                "${recipes.indexOf(recipe)}: $recipe"
+            }
+        }
+    }
+
+    fun listRecipesByDiet(diet: String): String {
+        val count = recipes.count { recipe: Recipe -> recipe.diet == diet }
+        return if (count == 0) {
+            "No recipes for diet type '$diet'"
+        } else {
+            val matches = recipes.filter { recipe: Recipe -> recipe.diet == diet }
+            matches.joinToString(separator = "\n") { recipe: Recipe ->
+                "${recipes.indexOf(recipe)}: $recipe"
+            }
+        }
+    }
+
+    fun listRecipesUnderTime(time: Int): String {
+        val count = recipes.count { recipe: Recipe -> recipe.prepTime + recipe.cookTime <= time }
+        return if (count == 0) {
+            "No recipes that can be made under $time"
+        } else {
+            val matches = recipes.filter { recipe: Recipe -> recipe.prepTime + recipe.cookTime <= time }
+            matches.joinToString(separator = "\n") { recipe: Recipe ->
+                "${recipes.indexOf(recipe)}: $recipe"
+            }
+        }
+    }
+
+    fun findRecipe(index: Int): Recipe? {
+        return if (isValidListIndex(index, recipes)) {
+            recipes[index]
+        } else {
+            null
+        }
+    }
+
+    fun findRecipeByName(name: String): String {
+        val matches = recipes.filter { recipe: Recipe -> recipe.recipeName.contains(name, true) }
+        return matches.joinToString(separator = "\n") { recipe: Recipe ->
+            "${recipes.indexOf(recipe)}: $recipe"
+        }
+    }
+
     fun numberOfRecipes(): Int {
         return recipes.size
     }
 
-    // TODO implement other CRUD methods
+    fun delete(index: Int): Recipe? {
+        return if (isValidListIndex(index, recipes)) {
+            recipes.removeAt(index)
+        } else {
+            null
+        }
+    }
+
+    fun update(indexToUpdate: Int, recipe: Recipe?): Boolean {
+        val recipeToUpdate = findRecipe(indexToUpdate)
+
+        if ((recipeToUpdate != null) && (recipe != null)) {
+            recipeToUpdate.recipeName = recipe.recipeName
+            recipeToUpdate.instructions = recipe.instructions
+            recipeToUpdate.diet = recipe.diet
+            recipeToUpdate.ingredients = recipe.ingredients
+            recipeToUpdate.prepTime = recipe.prepTime
+            recipeToUpdate.cookTime = recipe.cookTime
+            recipeToUpdate.mealType = recipe.mealType
+            return true
+        }
+        return false
+    }
+
+    private fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
 }
