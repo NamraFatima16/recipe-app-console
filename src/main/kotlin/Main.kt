1a6c87585233c828a3deab651b2e7425fd3d6b03
Namra <20094326@mail.wit.ie>
2022-05-03 11:41:29 +0100

    Implemented full UI and corresponding methods.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
+import controllers.IngredientAPI
 import controllers.RecipeAPI
+import models.Ingredient
+import models.Recipe
 import persistence.XMLSerializer
 import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
 import java.io.File
 import kotlin.system.exitProcess
 
@@ -15,8 +19,12 @@ fun runMenu() {
         when (val option = mainMenu()) {
             1 -> addRecipe()
             2 -> listAllRecipes()
-            3 -> updateRecipe()
-            4 -> deleteRecipe()
+            3 -> listRecipesByMealType()
+            4 -> listRecipesByDiet()
+            5 -> listRecipesUnderTime()
+            6 -> updateRecipe()
+            7 -> deleteRecipe()
+            8 -> searchByName()
             20 -> saveRecipes()
             21 -> loadRecipes()
             0 -> exitApp()
@@ -34,8 +42,12 @@ fun mainMenu(): Int {
          > | MAIN MENU                        |
          > |   1) Add a recipe                |
          > |   2) List all recipes            |
-         > |   3) Update a recipe             |
-         > |   4) Delete a recipe             |
+         > |   3) List all recipes for meal   |
+         > |   4) List all recipes with diet  |
+         > |   5) List all recipes under time |
+         > |   6) Update a recipe             |
+         > |   7) Delete a recipe             |
+         > |   8) Find a recipe by name       |
          > |   20) Save recipes               |
          > |   21) Load recipes               |
          > ------------------------------------
@@ -45,28 +57,134 @@ fun mainMenu(): Int {
     )
 }
 
-fun loadRecipes() {
-    TODO("Not yet implemented")
-}
+fun addRecipe() {
+    val ingredients = IngredientAPI()
+    val recipeName = readNextLine("Enter the name of the recipe: ")
+    val prepTime = readNextInt("Enter the prep time (in mins): ")
+    val cookTime = readNextInt("Enter the cook time (in mins): ")
+    val mealType = readNextLine("Enter the recipe meal type: ")
+    val diet = readNextLine("Enter the diet (vegan, vegetarian or meat): ")
+    val instruction = readNextLine("Enter the instructions: ")
 
-fun saveRecipes() {
-    TODO("Not yet implemented")
+    val numIngredients = readNextInt("Enter the number of ingredients in the recipe: ")
+    repeat(numIngredients) {
+        val ingrName = readNextLine("Enter the name of the ingredient: ")
+        val ingrQuantity = readNextLine("Enter the quantity: ")
+        val isAdded = ingredients.add(Ingredient(ingrName, ingrQuantity))
+        if (!isAdded) {
+            println("Adding Ingredient failed")
+        }
+    }
+
+    val isAdded = recipeApi.add(
+        Recipe(
+            recipeName, instruction, prepTime, cookTime, mealType, diet, ingredients
+        )
+    )
+    if (isAdded) {
+        println("Added Successfully")
+    } else {
+        println("Add Failed")
+    }
 }
 
 fun deleteRecipe() {
-    TODO("Not yet implemented")
+    listAllRecipes()
+    if (recipeApi.numberOfRecipes() > 0) {
+        val indexToDelete = readNextInt("Enter the index of the recipe to delete: ")
+        if (recipeApi.isValidIndex(indexToDelete)) {
+            val recipeToDelete = recipeApi.delete(indexToDelete)
+            if (recipeToDelete != null) {
+                println("Delete successful! Deleted recipe: ${recipeToDelete.recipeName}")
+            } else {
+                println("Delete failed")
+            }
+        } else {
+            println("There are no recipes for this index number $indexToDelete")
+        }
+    }
 }
 
 fun updateRecipe() {
-    TODO("Not yet implemented")
+    listAllRecipes()
+    if (recipeApi.numberOfRecipes() > 0) {
+        val indexToUpdate = readNextInt("Enter the index of the recipe to update: ")
+        if (recipeApi.isValidIndex(indexToUpdate)) {
+            val ingredients = IngredientAPI()
+            val recipeName = readNextLine("Enter the name of the recipe: ")
+            val prepTime = readNextInt("Enter the prep time (in mins): ")
+            val cookTime = readNextInt("Enter the cook time (in mins): ")
+            val mealType = readNextLine("Enter the recipe meal type: ")
+            val diet = readNextLine("Enter the diet (vegan, vegetarian or meat): ")
+            val instruction = readNextLine("Enter the instructions: ")
+
+            val numIngredients = readNextInt("Enter the number of ingredients in the recipe: ")
+            repeat(numIngredients) {
+                val ingrName = readNextLine("Enter the name of the ingredient: ")
+                val ingrQuantity = readNextLine("Enter the quantity: ")
+                val isAdded = ingredients.add(Ingredient(ingrName, ingrQuantity))
+                if (!isAdded) {
+                    println("Adding Ingredient failed")
+                }
+            }
+
+            val updateSuccess = recipeApi.update(
+                indexToUpdate, Recipe(recipeName, instruction, prepTime, cookTime, mealType, diet, ingredients)
+            )
+            if (updateSuccess) {
+                println("Update successful")
+            } else {
+                println("Update failed")
+            }
+        } else {
+            println("There are no recipes for this index number $indexToUpdate")
+        }
+    }
 }
 
 fun listAllRecipes() {
-    TODO("Not yet implemented")
+    println(recipeApi.listAllRecipes())
 }
 
-fun addRecipe() {
-    TODO("Not yet implemented")
+fun listRecipesUnderTime() {
+    val time = readNextInt("Enter total time in mins: ")
+    println("Following recipes can be made in under $time mins")
+    println(recipeApi.listRecipesUnderTime(time))
+}
+
+fun listRecipesByDiet() {
+    val diet = readNextLine("Enter the diet type(s): ")
+    println("Following recipes were found for diet type $diet")
+    println(recipeApi.listRecipesByDiet(diet))
+}
+
+fun listRecipesByMealType() {
+    val meal = readNextLine("Enter the meal type(s): ")
+    println("Following recipes were found for meal type $meal")
+    println(recipeApi.listRecipesByMealType(meal))
+}
+
+fun searchByName() {
+    val recipeName = readNextLine("Enter the name to search: ")
+    println("Following recipes were found for with name containg $recipeName")
+    println(recipeApi.findRecipeByName(recipeName))
+}
+
+fun loadRecipes() {
+    try {
+        recipeApi.load()
+    } catch (e: Exception) {
+        System.err.println("Error reading from file: $e")
+    }
+    println("Loaded ${recipeApi.numberOfRecipes()} from file")
+}
+
+fun saveRecipes() {
+    try {
+        recipeApi.store()
+    } catch (e: Exception) {
+        System.err.println("Error writing to file: $e")
+    }
 }
 
 fun exitApp() {
